92168859a737fd7cc47f5028760bef6e8a3112af
Nir Adler <niradler55@gmail.com>
2019-04-27 18:44:11 +0300

    new lib for fetch

--- src/import.js
+++ src/import.js
@@ -1,43 +1,31 @@
-const shell = require('shelljs');
-const fs = require('fs');
-const dir = './static/stages/';
+const fs = require("fs");
+const { getExportAndSave, getRestApis } = require("apigateway-export-tool");
+const dir = "./static/stages/";
 
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-}
-
-const getSwaggerFileById = (item) => `aws apigateway get-export --parameters extensions='postman' --rest-api-id ${item.id} --stage-name ${item.stage} --export-type swagger ./static/stages/${item.stage}/${item.name}.json`
-
-const run = (cmd) => {
-    return new Promise((resolve,reject)=>
-    shell.exec(cmd, (code, output) => {        
-        if (code !== 0) {
-            reject(code)
-            shell.exit(1)
-        }
-        resolve(output)
-    }))
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
 }
 
 const importFiles = async () => {
-try {
-    if (!shell.which('aws')) throw new Error('this script require aws cli');
-    const json = await run('aws apigateway get-rest-apis')
-    const apis = JSON.parse(json);
+  try {
+    const apis = await getRestApis();
     for (let i = 0; i < apis.items.length; i++) {
-        const item = apis.items[i];
-        item.stage = item.name.slice(0,item.name.indexOf('-'))
-        const path = dir + item.stage;
-        if (!fs.existsSync(path)){
-            fs.mkdirSync(path);
-        }
-        await run(getSwaggerFileById(item))
+      const item = apis.items[i];
+      item.stage = item.name.slice(0, item.name.indexOf("-"));
+      const path = dir + item.stage;
+      if (!fs.existsSync(path)) {
+        fs.mkdirSync(path);
+      }
+      await getExportAndSave(
+        { restApiId: item.id, stageName: item.stage },
+        path
+      );
     }
-    
+
     return true;
-} catch (error) {
+  } catch (error) {
     throw error;
-}
-}
+  }
+};
 
 module.exports = importFiles;
