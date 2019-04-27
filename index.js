7f5c18c720a339b3ce129042495ce276322571bf
Nir Adler <niradler55@gmail.com>
2019-04-27 20:21:29 +0300

    cli init

--- index.js
+++ index.js
@@ -1,81 +1,79 @@
-require('dotenv').config()
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const utils = require('./src/utils');
-const importFiles = require('./src/import');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const utils = require("./src/utils");
+const importFiles = require("./src/import");
 
 const app = express();
 const router = express.Router();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, "static")));
 
 const handleError = (error, req, res) => {
-    res.locals.message = error.message;
-    res.locals.error = error;
-    res.status(error.status || 500);
-    res.render('error', {error});
-}
+  res.locals.message = error.message;
+  res.locals.error = error;
+  res.status(error.status || 500);
+  res.render("error", { error });
+};
 
 app.use((err, req, res, next) => {
+  handleError(error, req, res);
+});
+
+router.get("/import", async (req, res) => {
+  try {
+    await importFiles();
+    res.redirect("/");
+  } catch (error) {
     handleError(error, req, res);
+  }
 });
-
-router.get('/import', async(req, res) => {
-    try {
-        await importFiles();
-        res.redirect('/');
-    } catch (error) {
-        handleError(error, req, res);
+router.get("/", async (req, res) => {
+  try {
+    const stages = {};
+    const stagesPath = "./static/stages";
+    stages.types = await utils.getFolderFilesList(stagesPath);
+    if (stages.types.length == 0) {
+      await importFiles();
+      stages.types = await utils.getFolderFilesList(stagesPath);
     }
-})
-router.get('/', async(req, res) => {
-    try {
-        const stages = {};
-        const stagesPath = './static/stages';
-        stages.types = await utils.getFolderFilesList(stagesPath);
-        if(stages.types.length == 0) {
-            await importFiles();  
-            stages.types = await utils.getFolderFilesList(stagesPath); 
-        }
-        for (let stage of stages.types) {
-            const files = await utils.getFolderFilesList(`${stagesPath}/${stage}`);
-            stages[stage] = {
-                files: files.map(f => `/${stage}/${f}`),
-                names: []
-            }
-            for (let pathToFile of stages[stage].files) {
-                const json = await utils.getFile(stagesPath + pathToFile);                
-                const obj = JSON.parse(json);
-                stages[stage]
-                    .names
-                    .push(obj.info.title)
-            }
-        }
-        res.render('index', {stages});
-    } catch (error) {
-        console.log({error})
-        handleError(error, req, res);
+    for (let stage of stages.types) {
+      const files = await utils.getFolderFilesList(`${stagesPath}/${stage}`);
+      stages[stage] = {
+        files: files.map(f => `/${stage}/${f}`),
+        names: []
+      };
+      for (let pathToFile of stages[stage].files) {
+        const json = await utils.getFile(stagesPath + pathToFile);
+        const obj = JSON.parse(json);
+        stages[stage].names.push(obj.info.title);
+      }
     }
-
+    res.render("index", { stages });
+  } catch (error) {
+    console.log({ error });
+    handleError(error, req, res);
+  }
 });
 
-router.get('/swagger', function (req, res) {
-    const {path} = req.query
-    const stagesPath = '/stages';
-    console.log(stagesPath + path)
-    res.render('swagger', {configFileLocation: stagesPath + path});
+router.get("/swagger", function(req, res) {
+  const { path } = req.query;
+  const stagesPath = "/stages";
+  console.log(stagesPath + path);
+  res.render("swagger", { configFileLocation: stagesPath + path });
 });
 
-app.use('/', router);
+app.use("/", router);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () =>
+  console.log(`micro-swagger running on port ${process.env.PORT}!`)
+);
---
+++ micro-swagger.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+const program = require("commander");
+const package = require("./package.json");
+const exec = require("child_process").exec;
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+const getEnv = () => {
+  const env = fs.readFileSync(__dirname + "/.env");
+  const buf = Buffer.from(env);
+  const config = dotenv.parse(buf);
+  return config;
+};
+
+const setEnv = config => {
+  let file = [];
+  for (const key in config) {
+    file.push(`${key}=${config[key]}`);
+  }
+  file.join("\n");
+  fs.writeFileSync(__dirname + "/.env", file);
+};
+
+const run = cmd =>
+  new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error !== null) {
+        reject(error);
+      }
+      resolve(stdout);
+    });
+  });
+
+program
+  .version(package.version)
+  .command("start")
+  .description("Start micro-swagger server.")
+  .option("-p, --port [port]", "port", 3055)
+  .action(args => {
+    const { port } = args;
+    const env = getEnv();
+    env["PORT"] = port;
+    setEnv(env);
+    run(`node ${__dirname}/index.js`)
+      .then(o => console.log(o))
+      .catch(e => console.log(e));
+  });
+
+program.parse(process.argv);
+
+program.on("command:*", function() {
+  console.error(
+    "Invalid command: %s\nSee --help for a list of available commands.",
+    program.args.join(" ")
+  );
+  process.exit(1);
+});
+
+if (!process.argv.slice(2).length) {
+  program.help();
+}
